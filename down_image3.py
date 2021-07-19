0b05376d97a9637593accb2e790719ece385cc83
zhangxin <zhangxin19870504@163.com>
2021-07-19 23:03:33 +0800

    校正数据

--- down_image3.py
+++ down_image3.py
@@ -9,37 +9,6 @@ import urllib.request
 from tqdm import tqdm
 import numpy as np
 import cv2
-'''
-urls = []
-
-# 训练集
-train = pd.concat([
-    pd.read_csv('input/Xeon1OCR_round1_train1_20210526.csv'),
-    pd.read_csv('input/Xeon1OCR_round1_train_20210524.csv')]
-)
-
-try:
-    os.mkdir('./train_data/tianchi/image/')
-except:
-    pass
-
-for row in train.iterrows():
-    path = json.loads(row[1]['原始数据'])['tfspath']
-    urls.append(path)
-    
-# 测试集
-train = pd.concat([
-    pd.read_csv('input/Xeon1OCR_round1_test1_20210528.csv'),
-    pd.read_csv('input/Xeon1OCR_round1_test2_20210528.csv'),
-    pd.read_csv('input/Xeon1OCR_round1_test3_20210528.csv')]
-)
-
-for row in train.iterrows():
-    path = json.loads(row[1]['原始数据'])['tfspath']
-    urls.append(path)
-
-print('Total images: ', len(urls))
-'''
 
 
 def down_image(url, out_dir):
@@ -53,31 +22,15 @@ def down_image(url, out_dir):
     urllib.request.urlretrieve(url, save_path)
 
 
-# from joblib import Parallel, delayed
-# Parallel(n_jobs=-1)(delayed(down_image)(url) for url in tqdm(urls))
-
-
-# for row in train.iterrows():
-#     path = json.loads(row[1]['原始数据'])['tfspath']
-#     print(path)
-#     if os.path.exists('./train_data/tianchi/image/' + path.split('/')[-1]):
-#         continue
-#     urllib.request.urlretrieve(path, './train_data/tianchi/image/' + path.split('/')[-1])
-
-
-def convert_tianchi_paddleocr(annot, img_path):
+def convert_tianchi_paddleocr(annot, img):
     if rotate_by_option:
-        img = cv2.imread(img_path)
         h, w = img.shape[:2]
         if annot[1] == '底部朝左':
             img = cv2.flip(cv2.transpose(img), 0)
-            cv2.imwrite(img_path, img)
         elif annot[1] == '底部朝上':
             img = cv2.flip(img, -1)
-            cv2.imwrite(img_path, img)
         elif annot[1] == '底部朝右':
             img = cv2.flip(cv2.transpose(img), 1)
-            cv2.imwrite(img_path, img)
 
     annot_ppocr = []
     for text_line in annot[0]:
@@ -98,24 +51,27 @@ def convert_tianchi_paddleocr(annot, img_path):
                 pts = [[y2, w - 1 - x2], [y3, w - 1 - x3],
                        [y4, w - 1 - x4], [y1, w - 1 - x1]]
         annot_ppocr.append({"transcription": transcription, "points": pts, "difficult": False})
-    return json.dumps(annot_ppocr, ensure_ascii=False)
+    return json.dumps(annot_ppocr, ensure_ascii=False), img
 
 
-def process_data(csv_path, out_img_dir):
+def process_data(csv_path, img_dir, src_img_dir, dst_img_dir):
     '''获取训练数据'''
+    in_img_dir = os.path.join(src_img_dir, img_dir)
+    out_img_dir = os.path.join(dst_img_dir, img_dir)
     if not os.path.isdir(out_img_dir):
         os.makedirs(out_img_dir)
 
     result = []
     for row in pd.read_csv(csv_path).iterrows():
         img_url = json.loads(row[1]['原始数据'])['tfspath']
-        img_path = os.path.join(out_img_dir, os.path.basename(img_url))
-        # if not os.path.isfile(img_path):
-        urllib.request.urlretrieve(img_url, img_path)
+        out_img_path = os.path.join(out_img_dir, os.path.basename(img_url))
+        src_img_path = os.path.join(in_img_dir, os.path.basename(img_url))
         if '融合答案' in row[1]:
             annot = json.loads(row[1]['融合答案'])
-            annot = convert_tianchi_paddleocr(annot, img_path)
-            result.append([img_path, annot])
+            img = cv2.imread(src_img_path, cv2.IMREAD_COLOR)
+            annot, img = convert_tianchi_paddleocr(annot, img)
+            cv2.imwrite(out_img_path, img)
+            result.append([out_img_path, annot])
     return result
 
 
@@ -141,30 +97,28 @@ def main(args):
     rotate_by_option = args.rotate_by_option.lower() == 'true'
     print(rotate_by_option)
 
-    csv_files = [('Xeon1OCR_round1_train1_20210526.csv', 'data/train1', 49),
-                 ('Xeon1OCR_round1_train2_20210526.csv', 'data/train2', 441),
-                 ('Xeon1OCR_round1_train_20210524.csv', 'data/train', 135)]
+    csv_files = [
+        ('Xeon1OCR_round1_train1_20210526.csv', 'train1', 49),
+        ('Xeon1OCR_round1_train2_20210526.csv', 'train2', 441),
+        ('Xeon1OCR_round1_train_20210524.csv', 'train', 135)
+    ]
     for name, img_dir, val_num in csv_files:
-        result = process_data('input/' + name, img_dir)
+        result = process_data('input/' + name, img_dir, args.src_img_dir, args.dst_img_dir)
         split_train_val(result,
-                        '{}/{}.train.txt'.format(args.train_val_dir, name),
-                        '{}/{}.val.txt'.format(args.train_val_dir, name), val_num)
-
-    csv_files = [('Xeon1OCR_round1_test1_20210528.csv', 'data/test1'),
-                 ('Xeon1OCR_round1_test2_20210528.csv', 'data/test2'),
-                 ('Xeon1OCR_round1_test3_20210528.csv', 'data/test3')]
-    for name, img_dir in csv_files:
-        process_data('input/' + name, img_dir)
+                        '{}/det_data/{}.train.txt'.format(args.dst_img_dir, name),
+                        '{}/det_data/{}.val.txt'.format(args.dst_img_dir, name),
+                        val_num)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_val_dir', default='data/det_data2')
-    parser.add_argument('--rotate_by_option', default='False')
+    parser.add_argument('--rotate_by_option', default='True')
+    parser.add_argument('--src_img_dir', default='./data')
+    parser.add_argument('--dst_img_dir', default='./data_calib')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     import argparse
-    rotate_by_option = False
+    rotate_by_option = True
     main(get_args())
